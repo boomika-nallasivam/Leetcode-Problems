22445483ae7ed59ca52fee1f27ea352482864e88
Boomika Nallasivam <knallasivam1981ed@gmail.com>
2025-11-28 22:31:38 +0530

    Add files via upload

---
+++ Leetcode 410.java
@@ -0,0 +1,38 @@
+class Solution {
+    public int splitArray(int[] nums, int k) {
+        int left = 0, right = 0;
+
+        for (int num : nums) {
+            left = Math.max(left, num); 
+            right += num;
+        }
+
+        while (left < right) {
+            int mid = left + (right - left) / 2;
+            if (canSplit(nums, k, mid)) {
+                right = mid;
+            } else {
+                left = mid + 1; 
+            }
+        }
+
+        return left;
+    }
+    private boolean canSplit(int[] nums, int k, int maxSum) {
+        int subarrays = 1;
+        int currentSum = 0;
+
+        for (int num : nums) {
+            if (currentSum + num > maxSum) {
+                subarrays++;
+                currentSum = num;
+
+                if (subarrays > k) return false;  
+            } else {
+                currentSum += num;
+            }
+        }
+        return true;
+    }
+
+}
