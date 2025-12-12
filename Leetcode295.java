8dd1cb4f9f0392d93e27c7e54c1de8bee7fe9e58
Boomika Nallasivam <knallasivam1981ed@gmail.com>
2025-12-12 23:21:45 +0530

    Add files via upload

---
+++ Leetcode295.java
@@ -0,0 +1,36 @@
+class MedianFinder {
+    PriorityQueue<Integer> minHeap = new PriorityQueue<>();
+    PriorityQueue<Integer> maxHeap = new PriorityQueue<>(Collections.reverseOrder());
+    public MedianFinder() {
+        
+    }
+    
+    public void addNum(int num) {
+        maxHeap.offer(num);
+        minHeap.offer(maxHeap.poll());
+        if (minHeap.size() > maxHeap.size())
+            maxHeap.offer(minHeap.poll());
+    }
+    
+    public double findMedian() {
+        if (maxHeap.size() > minHeap.size()) return maxHeap.peek();
+        return (minHeap.peek() + maxHeap.peek()) / 2.0d;
+    }
+}
+
+public class Main {
+    public static void main(String[] args) {
+        MedianFinder mf = new MedianFinder();
+
+        mf.addNum(1);
+        mf.addNum(2);
+        System.out.println(mf.findMedian()); // 1.5
+
+        mf.addNum(3);
+        System.out.println(mf.findMedian()); // 2.0
+
+        mf.addNum(5);
+        mf.addNum(4);
+        System.out.println(mf.findMedian()); // 3.0
+    }
+}
