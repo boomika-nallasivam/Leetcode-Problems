7db94d6e6f2664ed896c5b9d8b9902b2ddd6a2c8
Boomika Nallasivam <knallasivam1981ed@gmail.com>
2026-01-22 21:09:07 +0530

    Add files via upload

---
+++ Leetcode347.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    vector<int> topKFrequent(vector<int>& nums, int k) {
+
+        unordered_map<int, int> freq;
+
+        for (int num : nums) {
+            freq[num]++;
+        }
+
+        vector<pair<int, int>> v;
+        for (auto it : freq) {
+            v.push_back({it.second, it.first});
+        }
+
+        sort(v.rbegin(), v.rend());
+
+        vector<int> result;
+        for (int i = 0; i < k; i++) {
+            result.push_back(v[i].second);
+        }
+
+        return result;
+    }
+};
