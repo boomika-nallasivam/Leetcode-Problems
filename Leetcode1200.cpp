9b4f9fb08afa537c6887044a45139fe01d57b6e7
Boomika Nallasivam <knallasivam1981ed@gmail.com>
2026-01-26 20:56:07 +0530

    Add files via upload

---
+++ Leetcode1200.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    vector<vector<int>> minimumAbsDifference(vector<int>& arr) {
+        sort(arr.begin() , arr.end());
+        int mind = INT_MAX;
+        vector<vector<int>> res;
+
+        for(int i=1 ; i<arr.size() ; i++){
+            int dif = arr[i] -arr[i-1];
+            if(dif < mind){
+                mind = dif;
+                res.clear();
+                res.push_back({arr[i-1],arr[i]});
+
+            }
+            else if(dif == mind){
+                res.push_back({arr[i-1],arr[i]});
+            }
+        }
+
+        return res;
+    }
+};
