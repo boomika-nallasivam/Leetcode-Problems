53080cf87bbb2cae1bb793fe84f1154689d1766d
Boomika Nallasivam <knallasivam1981ed@gmail.com>
2026-03-14 11:33:27 +0530

    Create Leetcode977.java

---
+++ Leetcode977.java
@@ -0,0 +1,11 @@
+class Solution {
+    public int[] sortedSquares(int[] nums) {
+        
+        int[] arr = new int[nums.length];
+        for(int i=0 ; i<nums.length ; i++){
+            arr[i] = Math.abs(nums[i])*Math.abs(nums[i]);
+        }
+        Arrays.sort(arr);
+        return arr;
+    }
+}
