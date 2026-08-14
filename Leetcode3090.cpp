d5fb828f65fddda5d57def989b2f53c7bf92f364
Boomika Nallasivam <knallasivam1981ed@gmail.com>
2026-08-14 10:29:32 +0530

    Create Leetcode3090.cpp

---
+++ Leetcode3090.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    int maximumLengthSubstring(string s) {
+        int freq[26] = {0};
+
+        int left = 0;
+        int maxi = 0;
+
+        for (int right = 0; right < s.size(); right++) {
+
+            freq[s[right] - 'a']++;
+
+            while (freq[s[right] - 'a'] > 2) {
+                freq[s[left] - 'a']--;
+                left++;
+            }
+
+            maxi = max(maxi, right - left + 1);
+        }
+
+        return maxi;
+    }
+};
